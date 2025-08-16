bf85bdd8b59dd7a5c79338db8134cb278e0b284d
Raiundo-beep <raimundoarmandocumbe9@gmail.com>
2025-08-16 12:00:34 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Este ficheiro contém apenas a sua configuração do Firebase.
+// É uma boa prática para manter o código organizado e seguro.
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD70Uto5Z8gAHclBNm9peW7FKd4pR1-sZA",
+    authDomain: "clickmoz-e7e6b.firebaseapp.com",
+    projectId: "clickmoz-e7e6b",
+    storageBucket: "clickmoz-e7e6b.firebasestorage.app",
+    messagingSenderId: "57111705800",
+    appId: "1:57111705800:web:6ff95f2ce510c0c09f8dfd",
+    measurementId: "G-GBLYFL5WNM"
+};
+
+export { firebaseConfig };
